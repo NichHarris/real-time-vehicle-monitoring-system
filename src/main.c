09b8dee5e21d469c9500145574d2116b3932bef4
Nicholas Kawwas <nicholas.b.kawwas@gmail.com>
2022-03-08 00:13:28 -0500

    Added explanations to create and activate timer, created outline for producer and consumer thread routines, and todos of reading and message passing

--- src/main.c
+++ src/main.c
@@ -19,17 +19,36 @@
 #define COL_CURRENT_GEAR 34
 #define COL_VEHICLE_SPEED 44
 
-// Array index for each variable
+// Define Constants
+#define ONE_THOUSAND	1000
+#define ONE_MILLION		1000000
+
+
+//-----
+// Define Thread Index for Each Producer 
 #define FUEL_CONSUMPTION 0
 #define ENGINE_SPEED 1
 #define ENGINE_COOLANT_TEMP 2
 #define CURRENT_GEAR 3
 #define VEHICLE_SPEED 4
 
-// Periods for all parameters
-#define PERIOD_LENGTH 5000000 // 5 s
+// Define Offset and Period for All Tasks
+// - Starting at 1s, Occuring Every 5s
+#define OFFSET 1000000 // 1 second
+#define PERIOD 5000000 // 5 seconds
 
+// Define Number of Producer Threads
+#define NUM_PRODUCER_THREADS 5
+//---
 
+/* int with # of seconds elapsed to update sensor data each 1 second */
+int currentTime = 0;
+
+//array with use to hold data produced by the producer threads
+double produced[NUM_COLUMNS];
+
+/* 2D Array containing recorded sensor information (the dataset read into memory) */
+double dataset[ROW_NUM][COL_NUM];
 
 // Two-dimensional array representing the recorded data for each variable
 float sensor_data[NUM_COLS][NUM_ROWS];
@@ -76,22 +95,10 @@ const char* getfield(char* line, int num) {
     return NULL;
 }
 
-// Define Constants
-#define ONE_THOUSAND	1000
-#define ONE_MILLION		1000000
-
-// Define Offset and Period in microseconds
-#define OFFSET 1000000
-#define PERIOD 5000000
-
-// Define Number of Producer Threads
-#define NUM_PRODUCER_THREADS 5
-
-
 // Define Global Thread Attribute to Specify Characteristics of POSIX (Portable Operating System Interface) Thread
 pthread_attr_t attr;
 
-// Define Global Signal Set
+// Define Global Signal Set to Specify Set of Signals Affected
 sigset_t sigst;
 
 int main (int argc, char *argv[]) {
@@ -118,12 +125,6 @@ int main (int argc, char *argv[]) {
 		printf("Error %d: Failed to create consumer thread! \n", rc);
 		return -1;
 	}
-
-	//Fuel consumption (0x01), 
-	//Engine Spped in RPM (0x02), 
-	//Engine Coolant Temperature (0x03), 
-	//Current Gear (0x04), 
-	//Vehicle Speed (0x05)	
 
     // Create Producers Threads
 	// - Pass Thread Index as Argument to Specify Desired Data
@@ -162,13 +163,41 @@ void *threadProducer (void *arg) {
 	// Print Producer Thread Number Passed from Arguments
 	printf("Producer Thread #%d Created!\n", arg);
 
+	// Define Variable of Interest using Passed Argument (voi)
+	int voi = arg; 
+	char fileName[];
+
 	while(1) {
 		// TODO: Produce Data based on Argument Passed
 		// Check arg and determine which file to read
 		// Then Perform Msg Passing
-		
-		
-		
+
+		// Determine File Name using Variable of Interest 
+		if(voi = 1) {
+			// Fuel Consumption (0x01)
+			fileName = "./"
+		} else if(voi = 2) {
+			// Engine Spped in RPM (0x02)
+			fileName = "./"
+		} else if(voi = 3) {
+			//Engine Coolant Temperature (0x03)
+			fileName = "./"
+		} else if(voi = 4) {
+			// Current Gear (0x04)
+			fileName = "./"
+		} else if(voi = 5) {
+			// Vehicle Speed (0x05)
+			fileName = "./"
+		} else {
+			printf(voi);
+			printf("WTFF")
+		}
+
+		// Read 
+
+
+		// Msg Pass
+
 		printf("Producing!")
 	}
 
@@ -191,21 +220,24 @@ void *threadConsumer (void *arg) {
 	return NULL;
 }
 
-// Wait for next activation adapted from timers_code.c
+// Wait for next activation signal adapted from timers_code.c
 static void wait_next_activation(void) {
     // Use Timer to Wait for Expiration Signal Before Executing Task
     // - Suspend Thread until Timer Sends Signal to Execute Thread
-	int dummy;
+	int dummy; //TODO: Ask why dummy is empty
 	sigwait(&sigst, &dummy);
 }
 
-// Start periodic timer adapted from timers_code.c
+// Create and Activate real-time timer to implement periodic tasks adapted from timers_code.c
 int start_periodic_timer(uint64_t offset, int period) {
 	// Instantiate Timer Thread Object with Unique Timer Id
     timer_t timer;
 	
-    // Specify Kind of Timer by Setting Timer Parameters
-    // Set it_value (Offset) and it_interval (Period)
+    // Instantiate Timer Specifications 
+	// -> Specifies Kind of Timer by Setting Timer Parameters
+    // Set it_value as offset and it_interval as period
+	// - Timer Goes Off/Triggered First Time at it_value (One Shot Value)
+	// - Timer Goes Off Trigger/Go Off Again Every it_interval - Reloads Timer with Relative Value (Reload Value)
     // Note: tv_sec Specifies Value in Seconds Position, tv_nsec Specifies Value in Nano Seconds Position
     struct itimerspec timer_spec;
 	timer_spec.it_value.tv_sec = offset / ONE_MILLION;
@@ -213,16 +245,18 @@ int start_periodic_timer(uint64_t offset, int period) {
 	timer_spec.it_interval.tv_sec = period / ONE_MILLION;
 	timer_spec.it_interval.tv_nsec = (period % ONE_MILLION) * ONE_THOUSAND;
 	
-    // Initialize, Add SIGALRM, and Block Signal Set
-    // ...
-    struct sigevent sigev;
+	// Add Signal (SIGALRM) to Signal Mask with sigaddset and Block All Other Signals with sigemptyset
+	// Block signals (SIG_BLOCK) while in critical section (cs) with sigprocmask
 	const int signal = SIGALRM;
 	sigemptyset(&sigst);
 	sigaddset(&sigst, signal);
 	sigprocmask(SIG_BLOCK, &sigst, NULL);
 	
-    // Set Signal Event to 0, Notify Function, and Signal Number
-    // ...
+   
+	// Instantiate Signal Event Structure (sigevent)
+	// -> Creates Notification Structure Using Signal Informing Kernel to Deliver Event
+	// Specify Signal Event Notify Function as SIGEV_SIGNAL and Signal Number as SIGALRM
+    struct sigevent sigev;
 	memset(&sigev, 0, sizeof(struct sigevent));
 	sigev.sigev_notify = SIGEV_SIGNAL;
 	sigev.sigev_signo = signal;
@@ -231,7 +265,7 @@ int start_periodic_timer(uint64_t offset, int period) {
     // - Using CLOCK_REALTIME with Custom sigev Struct 
 	int res = timer_create(CLOCK_REALTIME, &sigev, &timer);
 	if (res < 0) {
-		perror("Error: Timer Creation Failed.");
+		perror("Error: Failed to create real-time timer!");
 		exit(-1);
 	}
 
@@ -240,26 +274,23 @@ int start_periodic_timer(uint64_t offset, int period) {
 	return timer_settime(timer, 0, &timer_spec, NULL);
 }
 
-// TODO: Update this
-// Task Main Function
+// Update Current Time using Real time Clock adapted from timers_code.c
 static void task_body(void) {
-	static int cycles = 0;
-	static uint64_t start;
 	uint64_t current;
 	struct timespec tv;
 	
-	if (start == 0) {
-		clock_gettime(CLOCK_REALTIME, &tv);
-		start = tv.tv_sec * ONE_THOUSAND + tv.tv_nsec / ONE_MILLION;
-	}
-	
 	clock_gettime(CLOCK_REALTIME, &tv);
-	current = tv.tv_sec * ONE_THOUSAND + tv.tv_nsec / ONE_MILLION;
-	
-	if (cycles > 0) {
-		fprintf(stderr, "Avg interval between instances: %f millisecons\n",
-			(double)(current-start)/cycles);
-	}
-	
-	cycles++;
+	current = tv.tv_sec * ONE_THOUSAND + tv.tv_nsec / ONE_MILLION;	
 }
+
+
+/*
+// TODO: Create Header File
+const char* getfield(char*, int);
+void extractParameterValues(int, int);
+void *threadProducer(void *);
+void *threadConsumer(void *);
+static void wait_next_activation();
+int start_periodic_timer(uint64_t, int);
+int task_body(long);
+*/
